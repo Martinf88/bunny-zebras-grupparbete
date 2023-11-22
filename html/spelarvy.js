f10691462ae803ea37d78fcbf2c6d7c903fe76fd
Kenny <kenny.moller.henriksson@gmail.com>
2023-11-22 11:52:22 +0100

    lade till filer och variabler

---
+++ html/spelarvy.js
@@ -0,0 +1,14 @@
+import { words } from './svenska-ord.js'
+
+const viewContainer = document.querySelector('.view-container')
+const hangMan = document.querySelector('.hangman')
+const scaffold = document.querySelector('#scaffold')
+const head = document.querySelector('#head')
+const body = document.querySelector('#body')
+const arms = document.querySelector('#arms')
+const legs = document.querySelector('#legs')
+const playerInput = document.querySelector('#player-input')
+const difficulty = document.querySelector('.difficulty')
+const normalBtn = document.querySelector('.normal')
+const theWord = document.querySelector('#the-word')
+const guesses = document.querySelector('#guesses')
