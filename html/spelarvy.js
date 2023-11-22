c3bcabfc5ee7934a0140e2bbdb8c3c6f5b6497f8
Kenny <kenny.moller.henriksson@gmail.com>
2023-11-22 13:33:48 +0100

    lite kod

--- html/spelarvy.js
+++ html/spelarvy.js
@@ -1,6 +1,8 @@
 import { words } from './svenska-ord.js'
 
 const viewContainer = document.querySelector('.view-container')
+const playContainer = document.querySelector('.play-container')
+const gamerContainer = document.querySelector('.gamer-container')
 const hangMan = document.querySelector('.hangman')
 const scaffold = document.querySelector('#scaffold')
 const head = document.querySelector('#head')
@@ -12,3 +14,16 @@ const difficulty = document.querySelector('.difficulty')
 const normalBtn = document.querySelector('.normal')
 const theWord = document.querySelector('#the-word')
 const guesses = document.querySelector('#guesses')
+const underline = document.querySelector('.underline')
+// const hidden = document.querySelector('.hidden')
+
+normalBtn.addEventListener('click', () => {
+	playContainer.classList.add('hidden')
+	
+	// theWord.style.visibility('hidden')
+	// theWord.classlist.add('hidden');
+})
+
+
+
+
