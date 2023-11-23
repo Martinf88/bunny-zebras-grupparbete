8f6e55b0d63a5c658df72491b1c290510b2a9efd
Martin Fahlström <makrill88@gmail.com>
2023-11-23 13:35:42 +0100

    win-message och loss-message hidden

--- html/game-over.js
+++ html/game-over.js
@@ -1,6 +1,12 @@
 import { randomWord } from './spelarvy.js'
 
 const wordReveal = document.querySelector('.word-reveal')
+const winMessage = document.querySelector('.win-message')
+const lossMessage = document.querySelector('.loss-message')
 
 wordReveal.innerText = `Ordet var: ${randomWord}`
 console.log(randomWord);
+
+
+winMessage.classList.add('hidden')
+lossMessage.classList.add('hidden')
