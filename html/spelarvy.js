a223147c3d94933c40a5c6e4f718b9a9b75e95e0
Martin Fahlström <makrill88@gmail.com>
2023-11-23 11:56:47 +0100

    kombinerat kod med kennys kod

--- html/spelarvy.js
+++ html/spelarvy.js
@@ -1,28 +1,93 @@
 import { words } from './svenska-ord.js'
 
-const viewContainer = document.querySelector('.view-container')
-const playContainer = document.querySelector('.play-container')
-const gamerContainer = document.querySelector('.gamer-container')
-const hangMan = document.querySelector('.hangman')
-const scaffold = document.querySelector('#scaffold')
-const head = document.querySelector('#head')
-const body = document.querySelector('#body')
-const arms = document.querySelector('#arms')
-const legs = document.querySelector('#legs')
-const playerInput = document.querySelector('#player-input')
-const difficulty = document.querySelector('.difficulty')
-const normalBtn = document.querySelector('.normal')
-const theWord = document.querySelector('#the-word')
-const guesses = document.querySelector('#guesses')
-const underline = document.querySelector('.underline')
+// const viewContainer = document.querySelector('.view-container')
+// const playContainer = document.querySelector('.play-container')
+// const gamerContainer = document.querySelector('.gamer-container')
+// const hangMan = document.querySelector('.hangman')
+// const scaffold = document.querySelector('#scaffold')
+// const head = document.querySelector('#head')
+// const body = document.querySelector('#body')
+// const arms = document.querySelector('#arms')
+// const legs = document.querySelector('#legs')
+// const playerInput = document.querySelector('#player-input')
+// const difficulty = document.querySelector('.difficulty')
+// const normalBtn = document.querySelector('.normal')
+// const theWord = document.querySelector('#the-word')
+// const guesses = document.querySelector('#guesses')
+// const underline = document.querySelector('.underline')
 // const hidden = document.querySelector('.hidden')
 
-normalBtn.addEventListener('click', () => {
-	playContainer.classList.add('hidden')
+// normalBtn.addEventListener('click', () => {
+// 	playContainer.classList.add('hidden')
 	
 	// theWord.style.visibility('hidden')
 	// theWord.classlist.add('hidden');
-})
+// })
+
+
+// theWord = value av math.random(words)
+// keyDown eller keyPress  för att dölja en bokstav i taget
+// flippa koden och dölj ordet för att avslöja en bokstav i taget
+// ordets längd skall vara minst tio tecken långt och vara kopplat till normal-knappen
+
+//senare skall figuren avslöjas bit för bit för varje korrekt gissning
+// senare skall varje knapptryckning visas i guesses
+// alla ord skall konverteras till lowercase
+
+
+const theWord = document.querySelector('#the-word');
+const randomIndex = Math.floor(Math.random() * words.length);
+const randomWord = words[randomIndex].toUpperCase();
+
+function generateWord() {
+	let hiddenLetters = Array(randomWord.length).fill('_');
+	
+	theWord.innerText = hiddenLetters.join(' ');
+	console.log(theWord);
+	
+	document.addEventListener('keydown', (event) => {
+	  const pressedKey = event.key.toUpperCase();
+	
+	  if (randomWord.includes(pressedKey)) {
+		randomWord.split('').forEach((char, index) => {
+		  if (char === pressedKey) {
+			hiddenLetters[index] = pressedKey;
+		  }
+		});
+		theWord.innerText = hiddenLetters.join(' ');
+	  }
+	
+	});
+	console.log('hej');
+}
+
+
+const playContainer = document.querySelector('.play-container')
+const normalBtn = document.querySelector('.normal')
+const playerInput = document.querySelector('#player-input')
+const gamerContainer = document.querySelector('.gamer-container')
+
+normalBtn.addEventListener('click', startGame );
+const playerNameContainer = document.createElement('div');
+playerNameContainer.className = 'page';
+playerNameContainer.style.display = 'none';
+gamerContainer.insertAdjacentElement('afterend', playerNameContainer);
+
+function startGame() {
+
+    const playerName = playerInput.value;
+    if (playerName.trim().length < 2) {
+        alert('Vänligen ange ditt namn innan du startar spelet. Minst två tecken.');
+        return;
+    }
+    playContainer.style.opacity = '0';
+    playerNameContainer.innerHTML = `<p id="player-name">Spelare: ${playerName}</p>`;
+    playerNameContainer.style.display = 'flex';
+
+	generateWord()
+}
+
+
 
 
 
--- html/the-word.js
+++ html/the-word.js
@@ -1,35 +1,65 @@
-import { words } from './svenska-ord.js'
+// import { words } from './svenska-ord.js'
 
-// theWord = value av math.random(words)
-// keyDown eller keyPress  för att dölja en bokstav i taget
-// flippa koden och dölj ordet för att avslöja en bokstav i taget
-// ordets längd skall vara minst tio tecken långt och vara kopplat till normal-knappen
+// // theWord = value av math.random(words)
+// // keyDown eller keyPress  för att dölja en bokstav i taget
+// // flippa koden och dölj ordet för att avslöja en bokstav i taget
+// // ordets längd skall vara minst tio tecken långt och vara kopplat till normal-knappen
 
-//senare skall figuren avslöjas bit för bit för varje korrekt gissning
-// senare skall varje knapptryckning visas i guesses
-// alla ord skall konverteras till lowercase
-
-
-
-const theWord = document.querySelector('#the-word');
-
-const randomIndex = Math.floor(Math.random() * words.length);
-const randomWord = words[randomIndex].toUpperCase();
+// //senare skall figuren avslöjas bit för bit för varje korrekt gissning
+// // senare skall varje knapptryckning visas i guesses
+// // alla ord skall konverteras till lowercase
 
 
-let hiddenLetters = Array(randomWord.length).fill('_');
+// const theWord = document.querySelector('#the-word');
+// const randomIndex = Math.floor(Math.random() * words.length);
+// const randomWord = words[randomIndex].toUpperCase();
 
-theWord.innerText = hiddenLetters.join(' ');
+// function generateWord() {
+// 	let hiddenLetters = Array(randomWord.length).fill('_');
+	
+// 	theWord.innerText = hiddenLetters.join(' ');
+// 	console.log(theWord);
+	
+// 	document.addEventListener('keydown', (event) => {
+// 	  const pressedKey = event.key.toUpperCase();
+	
+// 	  if (randomWord.includes(pressedKey)) {
+// 		randomWord.split('').forEach((char, index) => {
+// 		  if (char === pressedKey) {
+// 			hiddenLetters[index] = pressedKey;
+// 		  }
+// 		});
+// 		theWord.innerText = hiddenLetters.join(' ');
+// 	  }
+	
+// 	});
+// 	console.log('hej');
+// }
 
-document.addEventListener('keydown', (event) => {
-  const pressedKey = event.key.toUpperCase();
 
-  if (randomWord.includes(pressedKey)) {
-    randomWord.split('').forEach((char, index) => {
-      if (char === pressedKey) {
-        hiddenLetters[index] = pressedKey;
-      }
-    });
-    theWord.innerText = hiddenLetters.join(' ');
-  }
-});
+// const playContainer = document.querySelector('.play-container')
+// const normalBtn = document.querySelector('.normal')
+// const playerInput = document.querySelector('#player-input')
+// const gamerContainer = document.querySelector('.gamer-container')
+
+// normalBtn.addEventListener('click', startGame );
+// const playerNameContainer = document.createElement('div');
+// playerNameContainer.className = 'page';
+// playerNameContainer.style.display = 'none';
+// gamerContainer.insertAdjacentElement('afterend', playerNameContainer);
+
+// function startGame() {
+
+//     const playerName = playerInput.value;
+//     if (playerName.trim().length < 2) {
+//         alert('Vänligen ange ditt namn innan du startar spelet. Minst två tecken.');
+//         return;
+//     }
+//     playContainer.style.opacity = '0';
+//     playerNameContainer.innerHTML = `<p id="player-name">Spelare: ${playerName}</p>`;
+//     playerNameContainer.style.display = 'flex';
+
+// 	generateWord()
+// }
+
+
