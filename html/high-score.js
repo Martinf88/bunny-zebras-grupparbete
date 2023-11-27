5d232e7ec3341d129e6bd270eb7676c8a512c987
Martin Fahlström <makrill88@gmail.com>
2023-11-27 09:12:33 +0100

    kommentarer

---
+++ html/high-score.js
@@ -0,0 +1,15 @@
+/* Poängsystem 
+
+lätt svårighetsgrad
+300p för korrekt gissat ord utan ett minsta fel
+-25p om du gissar fel bokstav
+
+Normal svårighetsgrad
+t.ex 500p för korrekt gissat ord utan ett minsta fel
+-50 för varje felgissning
+
+svår svårighetsgrad
+750p för korrekt gissat ord utan minsta fel
+-50p förvarje felgissning
+
+*/
