30acd41ec2de49190d6a6398ded4fd859a239642
Martin Fahlström <makrill88@gmail.com>
2023-11-27 09:28:59 +0100

    mer kommentarer gällande poäng

--- html/high-score.js
+++ html/high-score.js
@@ -4,20 +4,22 @@ Användaren ska kunna växla mellan spelvy och poängvy. Poängvyn visar resulta
 Listan ska vara sorterad så att de bästa omgångarna visas överst: i första hand stigande på antal gissningar, i andra hand fallande på datum och tid för omgången. Man ska kunna växla till att visa omgångarna sorterade i fallande ordning på datum och tid.
 
 
+En high-scorelista för normal svårgihetsgrad och en för lätt
+
 
 Poängsystem 
 
 lätt svårighetsgrad
 300p för korrekt gissat ord utan ett minsta fel
--25p om du gissar fel bokstav
+-50p om du gissar fel bokstav
+mellan 6 - 9 bokstäver
+
 
 Normal svårighetsgrad
-t.ex 500p för korrekt gissat ord utan ett minsta fel
+t.ex 300p för korrekt gissat ord utan ett minsta fel
 -50 för varje felgissning
+mellan 10 - 14 bokstäver
 
-svår svårighetsgrad
-750p för korrekt gissat ord utan minsta fel
--50p förvarje felgissning
 
 */
 
