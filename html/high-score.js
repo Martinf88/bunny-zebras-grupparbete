2b28997b765c5b643f97afc9c91a3dbf7eae2c20
Martin Fahlström <makrill88@gmail.com>
2023-11-27 09:17:08 +0100

    fler kommentarer

--- html/high-score.js
+++ html/high-score.js
@@ -1,4 +1,11 @@
-/* Poängsystem 
+/* 
+Användaren ska kunna växla mellan spelvy och poängvy. Poängvyn visar resultatet från tidigare omgångar: spelarens namn, antalet felaktiga gissningar, ordets längd, datum+tid för omgången, och om man vann eller förlorade. Använd localStorage för att spara datan i webbläsaren.
+
+Listan ska vara sorterad så att de bästa omgångarna visas överst: i första hand stigande på antal gissningar, i andra hand fallande på datum och tid för omgången. Man ska kunna växla till att visa omgångarna sorterade i fallande ordning på datum och tid.
+
+
+
+Poängsystem 
 
 lätt svårighetsgrad
 300p för korrekt gissat ord utan ett minsta fel
@@ -13,3 +20,4 @@ svår svårighetsgrad
 -50p förvarje felgissning
 
 */
+
